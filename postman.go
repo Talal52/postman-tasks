08b61163a06611f0ad0b1b63fd21c64bc09ef8b3
talalafzal <talalafzal57@gmail.com>
2023-05-03 06:21:27 -0700

    GET, DELETE, PUT, POST in Postman

--- postman.go
+++ postman.go
@@ -10,92 +10,93 @@ type response struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
+	var data = make(map[string]string)
 
-func main() {
-	data := make(map[string]string)
-	router := gin.Default()
-
-	router.GET("/key/:input", func(c *gin.Context) {
-		input := c.Param("input")
-		value, exists := data[input]
-		if exists {
-			c.JSON(http.StatusOK, gin.H{
-				"key":   input,
-				"value": value,
-			})
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error message": "entered Key is not found!!!",
-			})
-		}
-	})
-
-	router.DELETE("/delete/:input", func(c *gin.Context) {
-		input := c.Param("input")
-		_, exists := data[input]
-		if exists {
-			delete(data, input)
-			c.JSON(http.StatusOK, gin.H{
-				"message": "deleted successfully",
-				"data":    data,
-			})
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "entered key not found",
-			})
-		}
-	})
-
-	router.PUT("/update/:req.key", func(c *gin.Context) {
-		//input := c.Param("input")
-		var req response
-		if err := c.BindJSON(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input",
-			})
-			return
-		}
-		_, exists := data[req.Key]
-		if exists {
-			updatedValue := req.Value
-			//data[req.Key] = updatedValue
-			data[req.Key] = updatedValue
-			c.JSON(http.StatusOK, gin.H{
-				"output": "successfully updated the entered value",
-				"data":   data,
-			})
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "entered key is not found",
-			})
-		}
-	})
-
-	router.POST("/store", func(c *gin.Context) {
-		var requestBody response
-		if err := c.BindJSON(&requestBody); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input",
-			})
-			return
-		}
-		data[requestBody.Key] = requestBody.Value
+func getKey(c *gin.Context) {
+	input := c.Param("input")
+	value, exists := data[input]
+	if exists {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Key-value pair added successfully",
+			"key":   input,
+			"value": value,
+		})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error message": "entered Key is not found!!!",
+		})
+	}
+}
+
+func deleteKey(c *gin.Context) {
+	input := c.Param("input")
+	_, exists := data[input]
+	if exists {
+		delete(data, input)
+		c.JSON(http.StatusOK, gin.H{
+			"message": "deleted successfully",
 			"data":    data,
 		})
-	})
-
-	router.GET("/display", func(c *gin.Context) {
-		var responseData []response
-		for key, value := range data {
-			responseData = append(responseData, response{key, value})
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "map key-values",
-			"data":    responseData,
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "entered key not found",
 		})
-	})
+	}
+}
 
+func updateKey(c *gin.Context) {
+	var req response
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid input",
+		})
+		return
+	}
+	_, exists := data[req.Key]
+	if exists {
+		data[req.Key] = req.Value
+		c.JSON(http.StatusOK, gin.H{
+			"output": "successfully updated the entered value",
+			"data":   data,
+		})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "entered key is not found",
+		})
+	}
+}
+
+func storeKey(c *gin.Context) {
+	var requestBody response
+	if err := c.BindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid input",
+		})
+		return
+	}
+	data[requestBody.Key] = requestBody.Value
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Key-value pair added successfully",
+		"data":    data,
+	})
+}
+
+func displayKeys(c *gin.Context) {
+	var responseData []response
+	for key, value := range data {
+		responseData = append(responseData, response{key, value})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "map key-values",
+		"data":    responseData,
+	})
+}
+func main() {
+	router := gin.Default()
+	router.GET("/key/:input", getKey)
+	router.DELETE("/delete/:input", deleteKey)
+	router.PUT("/update/:req.key", updateKey)
+	router.POST("/store", storeKey)
+	router.GET("/display", displayKeys)
+	
 	router.Run(":8080")
 }
